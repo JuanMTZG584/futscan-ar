7890d14979246b59e037b376a48a6927da3c85c8
Juan Martinez <juanmtzg584@gmail.com>
2026-04-27 16:48:09 -0600

    Corrección: Agregado de seleccion.html como archivo de entrada

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+
   server: {
     host: true,
-    allowedHosts: ['sloane-orthostichous-saul.ngrok-free.dev', 'apollo-separated-elements-link.trycloudflare.com']
+    allowedHosts: [
+      'sloane-orthostichous-saul.ngrok-free.dev',
+      'apollo-separated-elements-link.trycloudflare.com'
+    ]
+  },
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        seleccion: resolve(__dirname, 'seleccion.html')
+      }
+    }
   }
-  
 })
